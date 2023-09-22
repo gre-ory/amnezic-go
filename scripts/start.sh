f19b4b61b5c0532aabf14aa4f927e823663ab276
Gregory VALIGIANI <97035355+gvaligiani@users.noreply.github.com>
2023-09-22 18:00:43 +0200

    [port] (#10)
    * [port]
    
    * [helper]
    
    ---------
    
    Co-authored-by: gre_ory <gregory.valigiani@gmail.com>

---
+++ scripts/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PHASE=$( echo ${1} | awk '{ print tolower($0); }' )
+if [[ "${PHASE}" == "" ]]; then
+    echo "missing phase!"
+    exit 1
+fi
+if [[ "${PHASE}" != "stg" && "${PHASE}" != "prd"  ]]; then
+    echo "invalid phase!"
+    exit 1
+fi
+
+echo "~> sudo systemctl start amnezic.${PHASE}.service"
+sudo systemctl start amnezic.${PHASE}.service
+
+echo "~> sudo systemctl status amnezic.${PHASE}.service"
+sudo systemctl status amnezic.${PHASE}.service
